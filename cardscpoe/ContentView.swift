ceff8888227c10f09509bde543a733f5417cc355
lyushenzhi <lyushenzhixu@gmail.com>
2026-03-12 12:56:24 +0800

    Initial Commit

---
+++ cardscpoe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cardscpoe
+//
+//  Created by reverse game on 2026/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cardscpoe/cardscpoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardscpoeApp.swift
+//  cardscpoe
+//
+//  Created by reverse game on 2026/3/12.
+//
+
+import SwiftUI
+
+@main
+struct cardscpoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
